d81e770beef9845f17fbd08e026b37186101ade9
Forvisk <adrianojr11@gmail.com>
2018-06-25 21:21:15 -0300

    ex 7 ePrimo ok

--- haskell/ListaExerciciosHaskell/ex07_ePrimo.hs
+++ haskell/ListaExerciciosHaskell/ex07_ePrimo.hs
@@ -2,5 +2,10 @@
 ePrimo :: Int -> Bool
 ePrimo 1 = False
 ePrimo 2 = True
-ePrimo n | (n mod 2) == 0 = False
- | otherwise = True
+ePrimo n | (mod n 2) == 0 = False
+ | otherwise = testaSePrimo n 3
+
+testaSePrimo :: Int -> Int -> Bool
+testaSePrimo n d | d > div n 2 = True
+testaSePrimo n d | (mod n d) == 0 = False
+ | otherwise = testaSePrimo n (d+2)
